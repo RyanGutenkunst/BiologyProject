ea583382da3a8d28e48d718562a0cf482377893c
Ryan Gutenkunst <rgutenk@arizona.edu>
2025-02-10 11:32:39 -0700

    Fix Q.html -> q.html in one subsection. Case problems may exist elsewhere...

--- process_html.py
+++ process_html.py
@@ -4,5 +4,5 @@ from pathlib import Path
 fnames = Path('').rglob('*.html')
 
 for fname in fnames:
-    command = 'sed -i "" "s/http:\/\/www.Pharmacy.Arizona.EDU\/centers\/tox_center\/swehsc\/index.html/https:\/\/swehsc.pharmacy.arizona.edu/g" {0}'.format(fname)
+    command = 'sed -i "" "s/Q.html/q.html/g" {0}'.format(fname)
     os.system(command)
