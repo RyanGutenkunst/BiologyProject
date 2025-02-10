62b368b819cf1a03867811d26aa89c7f45a9e736
Ryan Gutenkunst <rgutenk@arizona.edu>
2025-02-10 11:36:12 -0700

    Convert q.html to Q.html in certain exercises.

--- process_html.py
+++ process_html.py
@@ -4,5 +4,5 @@ from pathlib import Path
 fnames = Path('').rglob('*.html')
 
 for fname in fnames:
-    command = 'sed -i "" "s/Q.html/q.html/g" {0}'.format(fname)
+    command = 'sed -i "" "s/q.html/Q.html/g" {0}'.format(fname)
     os.system(command)
