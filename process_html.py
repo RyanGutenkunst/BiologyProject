85c368d02aa10a92258a5813fa990122e572ec66
Ryan Gutenkunst <rgutenk@arizona.edu>
2025-02-15 13:17:23 -0700

    More removal of Code Fusion activities that no longer work.

--- process_html.py
+++ process_html.py
@@ -6,3 +6,7 @@ fnames = Path('').rglob('*.html')
 for fname in fnames:
     command = 'sed -i "" "s/q.html/Q.html/g" {0}'.format(fname)
     os.system(command)
+
+# Cold Fusion:
+./human_bio/problem_sets/blood_types/inherited.html
+./immunology/activities/AIDS2003/directions.html
